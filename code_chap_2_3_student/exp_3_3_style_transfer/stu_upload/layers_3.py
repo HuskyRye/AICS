8699062020a32926f4df3d7fea8e132603f495f2
HuskyRye <1412167533@qq.com>
2024-04-05 16:48:31 +0800

    Format exp_3_3.

--- code_chap_2_3_student/exp_3_3_style_transfer/stu_upload/layers_3.py
+++ code_chap_2_3_student/exp_3_3_style_transfer/stu_upload/layers_3.py
@@ -4,30 +4,41 @@ import os
 import scipy.io
 import time
 
+
 class ContentLossLayer(object):
     def __init__(self):
-        print('\tContent loss layer.')
+        print("\tContent loss layer.")
+
     def forward(self, input_layer, content_layer):
-         # TODO： 计算风格迁移图像和目标内容图像的内容损失
-    
+        # TODO： 计算风格迁移图像和目标内容图像的内容损失
+
         loss = _____________________________
         return loss
+
     def backward(self, input_layer, content_layer):
         # TODO： 计算内容损失的反向传播
         bottom_diff = _____________________
         return bottom_diff
 
+
 class StyleLossLayer(object):
     def __init__(self):
-        print('\tStyle loss layer.')
+        print("\tStyle loss layer.")
+
     def forward(self, input_layer, style_layer):
         # TODO： 计算风格迁移图像和目标风格图像的Gram 矩阵
-        style_layer_reshape = np.reshape(style_layer, [style_layer.shape[0], style_layer.shape[1], -1])
+        style_layer_reshape = np.reshape(
+            style_layer, [style_layer.shape[0], style_layer.shape[1], -1]
+        )
         self.gram_style = ____________________________
-        self.input_layer_reshape = np.reshape(input_layer, [input_layer.shape[0], input_layer.shape[1], -1])
-        self.gram_input = np.zeros([input_layer.shape[0], input_layer.shape[1], input_layer.shape[1]])
+        self.input_layer_reshape = np.reshape(
+            input_layer, [input_layer.shape[0], input_layer.shape[1], -1]
+        )
+        self.gram_input = np.zeros(
+            [input_layer.shape[0], input_layer.shape[1], input_layer.shape[1]]
+        )
         for idxn in range(input_layer.shape[0]):
-            self.gram_input[idxn, :, :] = ___________________        
+            self.gram_input[idxn, :, :] = ___________________
         M = input_layer.shape[2] * input_layer.shape[3]
         N = input_layer.shape[1]
         self.div = M * M * N * N
@@ -35,8 +46,15 @@ class StyleLossLayer(object):
         style_diff = ______________________
         loss = ______________________
         return loss
+
     def backward(self, input_layer, style_layer):
-        bottom_diff = np.zeros([input_layer.shape[0], input_layer.shape[1], input_layer.shape[2]*input_layer.shape[3]])
+        bottom_diff = np.zeros(
+            [
+                input_layer.shape[0],
+                input_layer.shape[1],
+                input_layer.shape[2] * input_layer.shape[3],
+            ]
+        )
         for idxn in range(input_layer.shape[0]):
             # TODO： 计算风格损失的反向传播
             bottom_diff[idxn, :, :] = __________________________

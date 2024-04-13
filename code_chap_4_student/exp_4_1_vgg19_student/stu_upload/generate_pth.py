7f107ca238b3d89cf71a7d03c983dcb1b9d1f5d9
HuskyRye <huskyrye@qq.com>
2024-04-13 17:45:36 +0800

    .

--- code_chap_4_student/exp_4_1_vgg19_student/stu_upload/generate_pth.py
+++ code_chap_4_student/exp_4_1_vgg19_student/stu_upload/generate_pth.py
@@ -6,10 +6,10 @@ from collections import OrderedDict
 
 os.putenv("MLU_VISIBLE_DEVICES", "")
 # fmt: off
-cfgs = [64,"R", 64,"R", "M", 128,"R", 128,"R", "M",
-       256,"R", 256,"R", 256,"R", 256,"R", "M", 
-       512,"R", 512,"R", 512,"R", 512,"R", "M",
-        512,"R", 512,"R", 512,"R", 512,"R", "M"]
+cfgs = [64, "R", 64, "R", "M", 128, "R", 128, "R", "M",
+       256, "R", 256, "R", 256, "R", 256, "R", "M",
+       512, "R", 512, "R", 512, "R", 512, "R", "M",
+        512, "R", 512, "R", 512, "R", 512, "R", "M"]
 # fmt: on
 IMAGE_PATH = "data/strawberries.jpg"
 VGG_PATH = "data/imagenet-vgg-verydeep-19.mat"
@@ -23,7 +23,7 @@ def vgg19():
         "conv3_1", "relu3_1", "conv3_2", "relu3_2", "conv3_3", "relu3_3", "conv3_4", "relu3_4", "pool3",
         "conv4_1", "relu4_1", "conv4_2", "relu4_2", "conv4_3", "relu4_3", "conv4_4", "relu4_4", "pool4",
         "conv5_1", "relu5_1", "conv5_2", "relu5_2", "conv5_3", "relu5_3", "conv5_4", "relu5_4", "pool5",
-        "flatten", "fc6", "relu6","fc7", "relu7", "fc8", "softmax"
+        "flatten", "fc6", "relu6", "fc7", "relu7", "fc8", "softmax"
     ]
     # fmt: on
     layer_container = nn.Sequential()

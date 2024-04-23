8fd06457dcbaf83d4d4eb6ebf81c57cdf432303d
HuskyRye <huskyrye@qq.com>
2024-04-23 17:32:18 +0800

    Finish exp_4_4.

--- code_chap_4_student/exp_4_4_custom_pytorch_op_student/stu_upload/op_hsigmoid/hsigmoid.cpp
+++ code_chap_4_student/exp_4_4_custom_pytorch_op_student/stu_upload/op_hsigmoid/hsigmoid.cpp
@@ -1,25 +1,30 @@
-//Pytorch扩展头文件的引用
-#include <torch/extension.h> 
-using namespace std; 
+// Pytorch扩展头文件的引用
+#include <torch/extension.h>
+using namespace std;
 
-//hsigmoid_cpu函数的具体实现
-torch::Tensor hsigmoid_cpu(const torch::Tensor & dets) {
-  //TODO: 将输入的tensor转化为浮点类型的vector
-  ______________________________________
-  int input_size = input_data.size(); 
-  //TODO: 创建一个浮点类型的output_data，output_data为大小与输入相同的vector
-  ______________________________________
-  //TODO: 对于输入向量的每个元素计算hsigmoid
-  ______________________________________
-  //TODO: Create tensor options with dtype float32
-  auto opts = torch::TensorOptions().dtype(torch::kFloat32);
-  //TODO: Create a tensor from the output vector
-  auto foo= torch::from_blob(output_data.data(), {int64_t(output_data.size())}, opts).clone();
-  //TODO: 将得到的tensor reshape为所需的大小
-  ______________________________________
-  return output;
-} 
-//TODO: 算子绑定为Pytorch的模块
-PYBIND11_MODULE(TORCH_EXTENSION_NAME, m) {	
-______________________________________
-}       
+// hsigmoid_cpu函数的具体实现
+torch::Tensor hsigmoid_cpu(const torch::Tensor& dets)
+{
+    // TODO: 将输入的tensor转化为浮点类型的vector
+    const float* data_ptr = dets.data_ptr<float>();
+    vector<float> input_data(data_ptr, data_ptr + dets.numel());
+    int input_size = input_data.size();
+    // TODO: 创建一个浮点类型的output_data，output_data为大小与输入相同的vector
+    vector<float> output_data(input_size);
+    // TODO: 对于输入向量的每个元素计算hsigmoid
+    for (int i = 0; i < input_size; ++i) {
+        output_data[i] = 1.0 / (1.0 + exp(-input_data[i]));
+    }
+    // TODO: Create tensor options with dtype float32
+    auto opts = torch::TensorOptions().dtype(torch::kFloat32);
+    // TODO: Create a tensor from the output vector
+    auto foo = torch::from_blob(output_data.data(), { int64_t(output_data.size()) }, opts).clone();
+    // TODO: 将得到的tensor reshape为所需的大小
+    auto output = foo.reshape(dets.sizes());
+    return output;
+}
+// TODO: 算子绑定为Pytorch的模块
+PYBIND11_MODULE(TORCH_EXTENSION_NAME, m)
+{
+    m.def("hsigmoid", &hsigmoid_cpu, "HSigmoid activation function on CPU");
+}
--- code_chap_4_student/exp_4_4_custom_pytorch_op_student/stu_upload/op_hsigmoid/setup.py
+++ code_chap_4_student/exp_4_4_custom_pytorch_op_student/stu_upload/op_hsigmoid/setup.py
@@ -2,17 +2,16 @@ from setuptools import setup
 from torch.utils import cpp_extension
 
 setup(
-    #TODO: 给出编译后的链接库名称
-    ______________________________________
+    # TODO: 给出编译后的链接库名称
+    name="hsigmoid_extension",
     ext_modules=[
         cpp_extension.CppExtension(
-    #TODO：以正确的格式给出编译文件即编译函数
-    ______________________________________
+            # TODO：以正确的格式给出编译文件即编译函数
+            name="hsigmoid_extension",  # 这是此扩展模块的名称
+            sources=["hsigmoid.cpp"],  # 这里列出所有需要编译的源代码文件
         )
     ],
     # 执行编译命令设置
-    cmdclass={						       
-        'build_ext': cpp_extension.BuildExtension
-    }
+    cmdclass={"build_ext": cpp_extension.BuildExtension},
 )
 print("generate .so PASS!\n")

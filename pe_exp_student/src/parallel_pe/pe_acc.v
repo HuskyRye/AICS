21bb82a9a71ffea8170cdf281ffd90cac136de47
HuskyRye <huskyrye@qq.com>
2024-05-15 20:12:41 +0800

    Init pe_exp.

---
+++ pe_exp_student/src/parallel_pe/pe_acc.v
@@ -0,0 +1,27 @@
+module pe_acc(
+  input  [1023:0] mult_result,
+  output [  31:0] acc_result
+);
+
+genvar i;
+genvar j;
+
+// *******************************************************************
+// int16 add tree
+// *******************************************************************
+wire [31:0] int16_result[5:0][31:0];
+for(i=0; i<=5; i=i+1)
+begin:int16_add_tree
+  for(j=0; j<(32/(2**i)); j=j+1)
+  begin:int16_adder
+    if(i==0) begin
+      assign int16_result[0][j] = ___________________________________;
+    end else begin
+      assign int16_result[i][j] = ___________________________________;
+    end
+  end
+end
+
+assign acc_result = int16_result[5][0];
+
+endmodule

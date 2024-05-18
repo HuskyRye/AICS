0e28f32554782267840613fc1a9b63552f79ebd8
HuskyRye <huskyrye@qq.com>
2024-05-18 23:29:28 +0800

    Finish matrix_pe.

--- pe_exp_student/src/matrix_pe/matrix_pe.v
+++ pe_exp_student/src/matrix_pe/matrix_pe.v
@@ -18,62 +18,62 @@ reg inst_vld;
 reg [7:0] inst;/*inst存放输入控制信号ib_ctl_uop的值*/
 always@(posedge clk or negedge rst_n) begin
 /* TODO: inst_vld & inst */
-  ___________________________________________________
-  ___________________________________________________
-  ___________________________________________________
-  ___________________________________________________
-  ___________________________________________________
-  ___________________________________________________
-  ___________________________________________________
-  ___________________________________________________
-  ___________________________________________________
+  if(!rst_n) begin
+    inst_vld <= 1'b0;
+    inst <= 8'h0;
+  end else if(ib_ctl_uop_valid && !inst_vld) begin
+    inst_vld <= 1'b1;
+    inst <= ib_ctl_uop;
+  end else if(ib_ctl_uop_ready) begin
+    inst_vld <= 1'b0;
+  end
 end
 wire pe_vld_i = inst_vld && nram_mpe_neuron_valid && wram_mpe_weight_valid;
 reg [7:0] iter;
 always@(posedge clk or negedge rst_n) begin
   /* TODO: iter */
-  ___________________________________________________
-  ___________________________________________________
-  ___________________________________________________
-  ___________________________________________________
-  ___________________________________________________
-  ___________________________________________________
-  end
+  if (!rst_n) begin
+    iter <= 8'h0;
+  end else if(pe_vld_i && iter < inst) begin
+    iter <= iter + 1;
+  end else if(iter == inst) begin
+    iter <= 8'h0;
+  end 
 end
 
 always@(posedge clk or negedge rst_n) begin
   /* TODO: ib_ctl_uop_ready */
-  ___________________________________________________
-  ___________________________________________________
-  ___________________________________________________
-  ___________________________________________________
-  ___________________________________________________
-  ___________________________________________________
-  ___________________________________________________
+  if (!rst_n) begin
+    ib_ctl_uop_ready <= 1'b0;
+  end else if (pe_vld_i && iter == inst - 1) begin
+    ib_ctl_uop_ready <= 1'b1;
+  end else if (ib_ctl_uop_ready) begin
+    ib_ctl_uop_ready <= 1'b0;
+  end
 end
 
 wire [511:0] pe_neuron = nram_mpe_neuron;
 wire [511:0] pe_weight = wram_mpe_weight;
 wire [1:0] pe_ctl;
-assign pe_ctl[0] = _________________________________;  /* TODO */
-assign pe_ctl[1] = _________________________________;  /* TODO */
+assign pe_ctl[0] = (iter == 0);  /* TODO */
+assign pe_ctl[1] = (iter == inst - 1);  /* TODO */
 
 wire [31:0] pe_result;
 wire pe_vld_o;
 parallel_pe u_parallel_pe (
 /* TODO */
-  ___________________________________________________
-  ___________________________________________________
-  ___________________________________________________
-  ___________________________________________________
-  ___________________________________________________
-  ___________________________________________________
-  ___________________________________________________
-  ___________________________________________________
+  .clk(clk),
+  .rst_n(rst_n),
+  .neuron(pe_neuron),
+  .weight(pe_weight),
+  .ctl(pe_ctl),
+  .vld_i(pe_vld_i),
+  .result(pe_result),
+  .vld_o(pe_vld_o)
 );
 
-assign nram_mpe_neuron_ready = _________________________________;  /* TODO */
-assign wram_mpe_weight_ready = _________________________________;  /* TODO */
+assign nram_mpe_neuron_ready = pe_vld_i;  /* TODO */
+assign wram_mpe_weight_ready = pe_vld_i;  /* TODO */
 
 assign result = pe_result;
 assign vld_o = pe_vld_o;
